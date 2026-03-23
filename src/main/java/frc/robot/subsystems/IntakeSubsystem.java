fcd2142d6f5cadbfa4d5431280bb8bb2bd754187
Jerry <jerryhxie@gmail.com>
2026-03-23 16:55:56 -0600

    shhhhh

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs.IntakeConfigs;
 import frc.robot.Constants.MotorConstants;
+
 // Clean
 public class IntakeSubsystem extends SubsystemBase{
 
