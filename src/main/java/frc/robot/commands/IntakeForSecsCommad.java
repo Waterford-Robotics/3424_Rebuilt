39ac2ff95936d5a513970233596401ca42375608
annika224 <ar100955@waterfordschool.org>
2026-03-23 19:19:29 -0600

    Changelog: --renamed constants for clariy, hard coded config constants - removed unnecessary code - edited commands - remived flip command - will add later TODO: ROBOT CONTAINER

--- src/main/java/frc/robot/commands/IntakeForSecsCommad.java
+++ src/main/java/frc/robot/commands/IntakeForSecsCommad.java
@@ -1,53 +1,56 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-// Spindexes
-// Good
+// Intakes!
+// Clean!
+
 package frc.robot.commands;
 
-import frc.robot.subsystems.SpindexerSubsystem;
+
+import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class SpindexerCommand extends Command {
+public class IntakeForSecsCommad extends Command {
 
-  // Uses Spindexer Subsystem
-  SpindexerSubsystem m_spindexerSubsystem;
+
+  // Uses Elevator and Subsystems
+  IntakeSubsystem m_intakeSubsystem;
   double m_seconds;
   Timer m_timer = new Timer();
 
+
   // Constructor
-  public SpindexerCommand(SpindexerSubsystem spindexerSubsystem, double seconds) {
-        
+  public IntakeForSecsCommad(IntakeSubsystem intakeSubsystem, double seconds) {
+       
     // Definitions and setting parameters are equal to members!
-    m_spindexerSubsystem = spindexerSubsystem;
-    addRequirements(spindexerSubsystem);
+    m_intakeSubsystem = intakeSubsystem;
+    addRequirements(intakeSubsystem);
     m_seconds = seconds;
   }
+
 
   // Reset timer when the command starts executing
   public void initialize() {
     m_timer.start();
     m_timer.reset();
   }
-  
+ 
   // Actual command
   public void execute() {
-
-    if(m_timer.get() < m_seconds) {
-      m_spindexerSubsystem.spindex();
-    }
+      m_intakeSubsystem.intake();
   }
+
 
   // Stuff that happens when command is over
   public void end(boolean interrupted) {
-    m_spindexerSubsystem.stopSpindex();
+    m_intakeSubsystem.stopIntake();
   }
+
 
   // Checks if the command is done
   public boolean isFinished() {
-
     // Am I done?  Am I done? Am I finally done?
-    return m_timer.get() > m_seconds;
+    return m_timer.hasElapsed(m_seconds);
   }
 }
--- src/main/java/frc/robot/commands/ShootForSecsCommand.java
+++ src/main/java/frc/robot/commands/ShootForSecsCommand.java
@@ -1,25 +1,28 @@
-
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+// Shoots - just runs shooter
+// Clean
 package frc.robot.commands;
 
-import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class IntakeFlipCommand extends Command {
 
-  IntakeSubsystem m_intakeSubsystem;
+public class ShootForSecsCommand extends Command {
+
+  ShooterSubsystem m_shooterSubsystem;
   double m_seconds;
-  int m_polarity;
   Timer m_timer = new Timer();
 
   // Constructor
-  public IntakeFlipCommand(IntakeSubsystem intakeSubsystem, double seconds, int polarity) {
+  public ShootForSecsCommand(ShooterSubsystem shooterSubsystem, double seconds) {
         
     // Definitions and setting parameters are equal to members!
-    m_intakeSubsystem = intakeSubsystem;
-    addRequirements(intakeSubsystem);
+    m_shooterSubsystem = shooterSubsystem;
+    addRequirements(shooterSubsystem);
     m_seconds = seconds;
-    m_polarity = polarity;
   }
 
   // Reset timer when the command starts executing
@@ -30,20 +33,17 @@ public class IntakeFlipCommand extends Command {
   
   // Actual command
   public void execute() {
-    if(m_timer.get() < m_seconds) {
-      m_intakeSubsystem.flipCommand(m_polarity);
-    }
+      m_shooterSubsystem.shoot();
   }
 
   // Stuff that happens when command is over
   public void end(boolean interrupted) {
-    m_intakeSubsystem.stopFlip();
+    m_shooterSubsystem.stopShooter();
   }
 
   // Checks if the command is done
   public boolean isFinished() {
-
     // Am I done?  Am I done? Am I finally done?
-    return m_timer.get() > m_seconds;
+    return m_timer.hasElapsed(m_seconds);
   }
 }
