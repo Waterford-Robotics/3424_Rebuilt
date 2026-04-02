ded828df94cdf2b556aeb11e10fe4fde1f3f4305
WaterfordProgrammers <seanjameson+3245@waterfordschool.org>
2026-04-01 18:49:46 -0600

    polarity changes in indexer and shooter -jesse

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -22,14 +22,14 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public void shoot() {
-    m_leftShooter.set(ShooterConstants.k_shooterSpeed);  // Revs up the two shooter motors
-    m_rightShooter.set(-ShooterConstants.k_shooterSpeed);
+    m_leftShooter.set(-ShooterConstants.k_shooterSpeed);  // Revs up the two shooter motors
+    m_rightShooter.set(ShooterConstants.k_shooterSpeed);
     
   }
 
   public void farShoot() {
-    m_leftShooter.set(1.3*ShooterConstants.k_shooterSpeed);  // Revs up the two shooter motors
-    m_rightShooter.set(-1.3*ShooterConstants.k_shooterSpeed);
+    m_leftShooter.set(-1.3*ShooterConstants.k_shooterSpeed);  // Revs up the two shooter motors
+    m_rightShooter.set(1.3*ShooterConstants.k_shooterSpeed);
     
   }
 
