07b883bca56c2547d6b4334cccfde1a5d2bcffa0
annika224 <ar100955@waterfordschool.org>
2026-04-12 13:45:06 -0600

    deleted uneeded imports

--- src/main/java/frc/robot/subsystems/IntakeFlipoutSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeFlipoutSubsystem.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs.IntakeConfigs;
 import frc.robot.Constants.MotorIDConstants;
-
 public class IntakeFlipoutSubsystem extends SubsystemBase {
 
     private TalonFX m_intakeFlipout;
